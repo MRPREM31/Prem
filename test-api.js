eccac52af08fa2c8d2a04f272e4dd8822272089d
MR.PREM <mr.prem2006@gmail.com>
2026-05-28 17:07:14 +0530

    feat: complete production cleanup and optimization for Vercel Hobby plan

--- test-api.js
+++
@@ -1,4 +0,0 @@
-fetch('https://prem-backend-9icx.onrender.com/api/projects')
-  .then(res => res.json())
-  .then(data => console.log('Success:', data))
-  .catch(err => console.error('Error:', err));
